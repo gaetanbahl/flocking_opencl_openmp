00cc04b56fb52e688e6df2f2def434dbb64977f3
Gaetan Bahl <gaetan.bahl@ensimag.grenoble-inp.fr>
2016-11-30 14:29:23 +0100

    multithreaded rendering

--- Makefile
+++ Makefile
@@ -1,17 +1,21 @@
 CC=g++
 CFLAGS=-Wall -O
-LDFLAGS=-lsfml-graphics -lsfml-window -lsfml-system
+LDFLAGS=-lsfml-graphics -lsfml-window -lsfml-system -lX11
 EXEC=bin/flocking
 SRCDIR=src
 BINDIR=bin
+INCLUDEDIR=include
 
 all: $(EXEC)
 
-bin/flocking: main.o
-	$(CC) -o $(BINDIR)/flocking $(BINDIR)/main.o $(LDFLAGS)
+bin/flocking: main.o boiddrawing.o
+	$(CC) -o $(BINDIR)/flocking $(BINDIR)/main.o $(BINDIR)/boiddrawing.o $(LDFLAGS)
 
-main.o: $(SRCDIR)/main.c
-	$(CC) -o $(BINDIR)/main.o -c $(SRCDIR)/main.c $(CFLAGS)
+main.o: $(SRCDIR)/main.c 
+	$(CC) -o $(BINDIR)/main.o -c $(SRCDIR)/main.c -I$(INCLUDEDIR)/* $(CFLAGS)
+
+boiddrawing.o: $(SRCDIR)/boiddrawing.cpp
+	$(CC) -o $(BINDIR)/boiddrawing.o -c $(SRCDIR)/boiddrawing.cpp -I$(INCLUDEDIR)/* $(CFLAGS)
 
 clean:
 	rm -f $(BINDIR)/*.o 
--- src/main.c
+++ src/main.c
@@ -1,10 +1,54 @@
 #include <SFML/Graphics.hpp>
+#include "../include/boiddrawing.h"
+#include <iostream>
+#include <X11/Xlib.h>
 
-int main()
+
+
+void renderLoop(sf::RenderWindow * window)
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+
+    //petit triangle
+    sf::CircleShape shape(10.f,3);
+    //shape.setFillColor(sf::Color::Green);
+
+    //chargement de la petite texture pour le triangle
+    sf::Texture txtr;
+    if(!txtr.loadFromFile("images/texture.png"))
+    {
+        std::cout << "could not load texture, exiting program" << std::endl;
+        return;
+    }
+    else
+    {
+        std::cout << "successfully loaded texture" << std::endl;
+    }
+
+    shape.setTexture(&txtr);
+
+    //boucle de rendu
+    while (window->isOpen())
+    {
+        window->clear();
+        window->draw(shape);
+        window->display();
+    }
+
+}
+
+
+int main(int argc, char ** argv)
+{
+    //Lancement du thread de rendu
+    XInitThreads();
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Flocking Simulation");
+    window.setActive(false);
+    sf::Thread thread(&renderLoop, &window);
+    thread.launch();
+
+    //on garde les dimensions de la fenêtre dans un coin, ca peut être utile
+    sf::Vector2u win_size = window.getSize();
+    std::cout << "Actual window size is " <<  win_size.x << " "  << win_size.y << std::endl;
 
     while (window.isOpen())
     {
@@ -15,9 +59,12 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        {
+            std::cout << "exiting program" << std::endl;
+            window.close();
+        }
     }
 
     return 0;
